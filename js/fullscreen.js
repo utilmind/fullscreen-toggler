40c026b52633c7984f4397365d9d537e192d45dd
Aleksey Kuznietsov <utilmind@gmail.com>
2020-07-19 07:30:26 -0400

    Add files via upload

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,3 +1,32 @@
+/**
+ *  FullScreen toggler
+ *  https://github.com/utilmind/fullscreen-toggler/
+ *
+ *  (c) 07.2020, Oleksii Kuznietsov aka UtilMind.
+ *  Free by MIT license.
+ *
+ *  Usage example:
+    ==============
+
+    HTML
+
+        <button class="btn btn-primary toggle-fullscreen" title="Full screen" data-toggle="tooltip">
+            <img src="images/expand.svg" alt="Full screen" />
+        </button>
+
+    JavaScript
+
+        var $fullScreenTogglers = $(".toggle-fullscreen");
+        $fullScreenTogglers.click(function(e) {
+            e.preventDefault(); // in case if toggler is an anchor
+            toggleFullScreen(function(isFullScreen) {
+                $fullScreenTogglers.find("img").attr("src", isFullScreen ? "images/compress.svg" : "images/expand.svg");
+            });
+        });
+
+    NOTE: This quick example uses jQuery. But the function itself written in vanilla JavaScript, so jQuery doesn't required.
+ */
+
 function toggleFullScreen(onToggle) {
     // Doc on the FullScreen feature: https://developers.google.com/web/fundamentals/native-hardware/fullscreen/
     var me = this, // this is window, global
