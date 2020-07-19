f05b7e9fe9d70dc7119ac1dd7f64ef8e9978a46d
Aleksey Kuznietsov <utilmind@gmail.com>
2020-07-19 07:29:38 -0400

    Add files via upload

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,35 +1,6 @@
-/**
- *  FullScreen toggler
- *  https://github.com/utilmind/fullscreen-toggler/
- *
- *  (c) 07.2020, Oleksii Kuznietsov aka UtilMind.
- *  Free by MIT license.
- *
- *  Usage example:
-    ==============
-
-    HTML
-
-        <button class="btn btn-primary toggle-fullscreen" title="Full screen" data-toggle="tooltip">
-            <img src="images/expand.svg" alt="Full screen" />
-        </button>
-
-    JavaScript
-
-        var $fullScreenTogglers = $(".toggle-fullscreen");
-        $fullScreenTogglers.click(function(e) {
-            e.preventDefault(); // in case if toggler is an anchor
-            toggleFullScreen(function(isFullScreen) {
-                $fullScreenTogglers.find("img").attr("src", isFullScreen ? "images/compress.svg" : "images/expand.svg");
-            });
-        });
-
-    NOTE: This quick example uses jQuery. But the function itself written in vanilla JavaScript, so jQuery doesn't required.
- */
-
 function toggleFullScreen(onToggle) {
     // Doc on the FullScreen feature: https://developers.google.com/web/fundamentals/native-hardware/fullscreen/
-    var me = this,
+    var me = this, // this is window, global
         doc = window.document, // window.document
         docEl = doc.documentElement,
         // find the function (AK 16.02.2020: we really need all these checking, otherwise it will not work at least on Edge)
@@ -42,6 +13,17 @@ function toggleFullScreen(onToggle) {
             return doc.fullscreenElement || doc.mozFullScreenElement || doc.webkitFullscreenElement || doc.msFullscreenElement ? 1 : 0;
         },
 
+        checkFullScreen = function() {
+            // in the end of resize process
+            setTimeout(function() {
+                var curFullScreen = isInFullScreen();
+                if (me._isInFullScr !== curFullScreen) {
+                  me._isInFullScr = curFullScreen;
+                  onToggle(curFullScreen);
+                }
+            }, 0);
+        },
+
         isFullScreen;
 
     // check if it's already in full screen
@@ -52,19 +34,13 @@ function toggleFullScreen(onToggle) {
 
     // in the end of process
     setTimeout(function() {
-        isFullScreen = isInFullScreen();
-        if (onToggle && ("undefined" === typeof me.rsz)) {
-            me.rsz = isFullScreen; // save current state
-            window.addEventListener("resize", function() {
-                // in the end of resize process
-                setTimeout(function() {
-                    var curFullScreen = isInFullScreen();
-                    if (me.rsz !== curFullScreen) {
-                      me.rsz = curFullScreen;
-                      onToggle(curFullScreen);
-                    }
-                }, 0);
+        if (onToggle) {
+          if ("undefined" === typeof me._isInFullScr) { // first time
+            checkFullScreen(); // first toggle before hooked resize event!
+            addEventListener("resize", function() { // window.addEventListener.
+                checkFullScreen();
             });
+          }
         }
     }, 0);
 }
